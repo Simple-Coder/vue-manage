4350718d4dac3089c38b6456d54fdd4ceec402ba
xiedong <dxiesjz@163.com>
2022-05-21 20:24:38 +0800

    菜单初步

--- vue-manage/src/components/CommonAside.vue
+++ vue-manage/src/components/CommonAside.vue
@@ -6,7 +6,16 @@
     @close="handleClose"
     :collapse="isCollapse"
   >
-    <el-submenu index="1">
+    <el-menu-item
+      v-for="item in noChildren"
+      :index="item.path"
+      :key="item.path"
+    >
+      <i class="'el-icon-' + item.icon"></i>
+      <span slot="title">{{ item.label }}</span>
+    </el-menu-item>
+
+    <el-submenu index="2">
       <template slot="title">
         <i class="el-icon-location"></i>
         <span slot="title">导航一</span>
@@ -16,26 +25,7 @@
         <el-menu-item index="1-1">选项1</el-menu-item>
         <el-menu-item index="1-2">选项2</el-menu-item>
       </el-menu-item-group>
-      <el-menu-item-group title="分组2">
-        <el-menu-item index="1-3">选项3</el-menu-item>
-      </el-menu-item-group>
-      <el-submenu index="1-4">
-        <span slot="title">选项4</span>
-        <el-menu-item index="1-4-1">选项1</el-menu-item>
-      </el-submenu>
     </el-submenu>
-    <el-menu-item index="2">
-      <i class="el-icon-menu"></i>
-      <span slot="title">导航二</span>
-    </el-menu-item>
-    <el-menu-item index="3" disabled>
-      <i class="el-icon-document"></i>
-      <span slot="title">导航三</span>
-    </el-menu-item>
-    <el-menu-item index="4">
-      <i class="el-icon-setting"></i>
-      <span slot="title">导航四</span>
-    </el-menu-item>
   </el-menu>
 </template>
 
@@ -43,7 +33,50 @@
 export default {
   data() {
     return {
-      isCollapse: true,
+      isCollapse: false,
+      menu: [
+        {
+          path: "/",
+          name: "Home",
+          label: "首页",
+          icon: "s-home",
+          url: "Home/Home",
+        },
+        {
+          path: "/mall",
+          name: "mall",
+          label: "商品管理",
+          icon: "video-play",
+          url: "MallManage/MallManage",
+        },
+        {
+          path: "/user",
+          name: "user",
+          label: "用户管理",
+          icon: "user",
+          url: "UserManage/UserManage",
+        },
+        {
+          label: "其他",
+          icon: "location",
+          children: [
+            {
+              path: "/page1",
+              name: "page1",
+              label: "页面1",
+              icon: "setting",
+              url: "Other/PageOne",
+            },
+            {
+              path: "/page2",
+              name: "page2",
+              label: "页面2",
+              icon: "setting",
+              url: "Other/PageTwo",
+            },
+          ],
+        },
+      ],
     };
   },
   methods: {
@@ -52,6 +85,14 @@ export default {
     },
     handleClose(key, keyPath) {
       console.log(key, keyPath);
+    },
+  },
+  computed: {
+    noChildren() {
+      return this.menu.filter((item) => !item.children);
+    },
+    hasChildren() {
+      return this.menu.filter((item) => item.children);
     },
   },
 };
