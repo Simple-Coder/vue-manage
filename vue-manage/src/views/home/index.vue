702b672cc7f10ac78ea5dc3ce44dc092bbed96a9
xiedong <dxiesjz@163.com>
2022-05-22 11:57:07 +0800

    首页admin布局

--- vue-manage/src/views/home/index.vue
+++ vue-manage/src/views/home/index.vue
@@ -1,14 +1,30 @@
 <template>
-  <div>
-    <p>我是home页面</p>
-  </div>
+  <el-row class="home" :gutter="20">
+    <el-col :span="8" style="margin-top: 20px">
+      <el-card shadow="hover">
+        <div class="user">
+          <img :src="userIcon" />
+          <div class="userinfo">
+            <p class="name">Admin</p>
+            <p class="access">超管</p>
+          </div>
+        </div>
+        <div class="login-info">
+          <p>上次登录时间：<span>2022-05-22</span></p>
+          <p>上次登录地点：<span>北京</span></p>
+        </div>
+      </el-card>
+    </el-col>
+  </el-row>
 </template>
 
 <script>
 export default {
-  name: "Home",
+  name: "home",
   data() {
-    return {};
+    return {
+      userIcon: require("@/assets/images/user.png"),
+    };
   },
 
   components: {},
