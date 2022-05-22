f85a9cd9702032132a950625d9567730872da6f2
xiedong <dxiesjz@163.com>
2022-05-22 12:02:29 +0800

    首页数据

--- vue-manage/src/views/home/index.vue
+++ vue-manage/src/views/home/index.vue
@@ -14,6 +14,16 @@
           <p>上次登录地点：<span>北京</span></p>
         </div>
       </el-card>
+      <el-card style="margin-top: 20px; height: 460px">
+        <el-table :data="tableData">
+          <el-table-column
+            v-for="(val, key) in tableLabel"
+            :key="key"
+            :label="val"
+            :prop="key"
+          ></el-table-column>
+        </el-table>
+      </el-card>
     </el-col>
   </el-row>
 </template>
@@ -24,6 +34,50 @@ export default {
   data() {
     return {
       userIcon: require("@/assets/images/user.png"),
+      tableData: [
+        {
+          name: "oppo",
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800,
+        },
+        {
+          name: "vivo",
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800,
+        },
+        {
+          name: "苹果",
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800,
+        },
+        {
+          name: "小米",
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800,
+        },
+        {
+          name: "三星",
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800,
+        },
+        {
+          name: "魅族",
+          todayBuy: 100,
+          monthBuy: 300,
+          totalBuy: 800,
+        },
+      ],
+      tableLabel: {
+        name: "课程",
+        todayBuy: "今日购买",
+        monthBuy: "本月购买",
+        totalBuy: "总购买",
+      },
     };
   },
 
