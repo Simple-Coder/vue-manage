0195353e983a5c1801c428e8c73b3d67cae05689
xiedong <dxiesjz@163.com>
2022-05-22 20:39:25 +0800

    、、

--- vue-manage/src/components/CommonTag.vue
+++ vue-manage/src/components/CommonTag.vue
@@ -1,59 +1,58 @@
 <template>
   <div class="tags">
     <el-tag
-        v-for="(tag, index) in tags"
-        :key="tag.name"
-        :closable="tag.name !== 'home'"
-        :effect="$route.name === tag.name ? 'dark' : 'plain'"
-        @click="changeMenu(tag)"
-        @close="handleClose(tag, index)"
-        size="small"
+      v-for="(tag, index) in tags"
+      :key="tag.name"
+      :closable="tag.name !== 'home'"
+      :effect="$route.name === tag.name ? 'dark' : 'plain'"
+      @click="changeMenu(tag)"
+      @close="handleClose(tag, index)"
+      size="small"
     >
-      {{tag.label}}
+      {{ tag.label }}
     </el-tag>
   </div>
 </template>
 
 <script>
-import {mapState, mapMutations} from 'vuex'
+import { mapState, mapMutations } from "vuex";
 
 export default {
-  name: 'CommonTag',
+  name: "CommonTag",
   data() {
-    return {
-
-    }
+    return {};
   },
   computed: {
     ...mapState({
-      tags: state => state.tab.tabsList
+      tags: (state) => state.tab.tabsList,
     }),
   },
   methods: {
     ...mapMutations({
-      close: 'closeTag'
+      close: "closeTag",
     }),
     changeMenu(item) {
-      this.$router.push({name: item.name})
+      this.$router.push({ name: item.name });
     },
     handleClose(tag, index) {
-      const length = this.tags.length - 1
-      this.close(tag)
+      const length = this.tags.length - 1;
+      this.close(tag);
       if (tag.name !== this.$route.name) {
-        return
+        return;
       }
+      //点击最右侧tag
       if (index === length) {
         this.$router.push({
-          name: this.tags[index - 1].name
-        })
+          name: this.tags[index - 1].name,
+        });
       } else {
         this.$router.push({
-          name: this.tags[index].name
-        })
+          name: this.tags[index].name,
+        });
       }
-    }
+    },
   },
-}
+};
 </script>
 
 <style lang="less" scoped>
